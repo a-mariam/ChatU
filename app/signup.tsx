81de02cff1299ace7e0d69d6d9dd2790e08b84f3
a-mariam <mariiam22222@gmail.com>
2025-09-13 17:40:51 +0100

    working on sign up page

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,9 +1,16 @@
-import {View} from "react-native";
+import {View,Text } from "react-native";
 import signUp from "@/components/signupcomponent";
 
 function Signup() {
     return (
         // <signUp/>
-        <View></View>
+        <View
+            className={` bg-red-300 h-full w-full px-2  `}
+        >
+            <Text>Home Screen</Text>
+
+        </View>
     )
 }
+
+export default Signup;

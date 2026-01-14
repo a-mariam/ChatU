46679a1ba1b5def21b5fbc5d8642ed8066c4e836
a-mariam <mariiam22222@gmail.com>
2026-01-14 12:47:51 +0100

    styling layout footer

--- app/home.tsx
+++ app/home.tsx
@@ -13,15 +13,23 @@ function Home  ()  {
 
     {/*</View>*/}
 
+    {/*<View className={`  justify-self-center w-[50%] bg-purple-100 `}>*/}
+    {/*    <Lineicons className={` mr-auto ml-auto `} icon={ChatBubble2Outlined} size={32} color="#007AFF" strokeWidth={2} />*/}
+    {/*</View>*/}
+
 
     return (
         <View className={` bg w-full h-full  `}>
-            <View className={` mt-auto   flex-row justify-between  w-full bg-red-200  `}>
-                <View className={` w-[50%] bg-purple-300 `}>
-                    <Lineicons icon={Home2Outlined} size={32} color="#007AFF" strokeWidth={2} />
+            <View className={` mt-auto h-[5rem]  flex-row justify-between  w-full bg-red-200  `}>
+                <View className={` w-[50%] hover:bg-purple-300 `}>
+                    <View className={` w-fit mt-auto mb-auto mr-auto ml-auto `}>
+                        <Lineicons className={` mr-auto ml-auto `} icon={Home2Outlined} size={32} color="#007AFF" strokeWidth={2} />
+                    </View>
                 </View>
-                <View className={`  justify-self-center w-[50%] bg-purple-100 `}>
-                    <Lineicons icon={ChatBubble2Outlined} size={32} color="#007AFF" strokeWidth={2} />
+                <View className={` w-[50%] hover:bg-purple-300 `}>
+                    <View className={` w-fit mt-auto mb-auto  mr-auto ml-auto `}>
+                        <Lineicons className={` mr-auto ml-auto `} icon={ChatBubble2Outlined} size={32} color="#007AFF" strokeWidth={2} />
+                    </View>
                 </View>
             </View>
         </View>

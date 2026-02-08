0d5326a591ed595af73bd555b89166646e3352eb
a-mariam <mariiam22222@gmail.com>
2026-02-08 13:30:58 +0100

    working message card

--- reuseables/MessageInfo.tsx
+++ reuseables/MessageInfo.tsx
@@ -1,4 +1,5 @@
 import {Pressable,StyleSheet, Text, TextInput, View} from "react-native";
+import { Avatar, Accessory } from "react-native-elements";
 
 
 interface IMessageInfo {
@@ -13,11 +14,31 @@ function MessageInfo ({id, message,userName}: IMessageInfo) {
         <View
             id={id}
             testID={id}
-            className={` w-full h-[8vh]  p-4 border-b border-b-grey500  `}
+            className={` flex-row gap-4  items-center bg-red-300  overflow-hidden `}
             style={styles.container}
         >
-            <Text>{message}</Text>
-            <Text>{userName}</Text>
+
+            <Avatar
+                size="medium"
+                rounded
+                title="MT"
+                onPress={() => console.log("Works!")}
+                activeOpacity={0.7}
+                containerStyle={{backgroundColor: 'purple'}}
+            />
+
+            <View className={` w-full grid bg-red-200`}>
+                {/*<Text style={styles.userName} >{userName}</Text>*/}
+                {/*<Text>{message}</Text>*/}
+                {/*<Text style={styles.time}>2:30am</Text>*/}
+                {/*<Text>{userName}</Text>*/}
+                <View className={`flex-row justify-between w-full  `}>
+                    <Text style={styles.userName} >{userName}</Text>
+                    <Text style={styles.time}>2:30am</Text>
+                </View>
+                <Text>{message}</Text>
+
+            </View>
         </View>
     )
 
@@ -25,9 +46,21 @@ function MessageInfo ({id, message,userName}: IMessageInfo) {
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height: 50,
-        paddingHorizontal: 6,
+        height: 100,
+        // paddingHorizontal:10,
+        paddingVertical: 10,
+    },
+    userName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    time: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        color: '#9333ea',
     }
+
 })
 
 
